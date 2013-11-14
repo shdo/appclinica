60b92430bdf1a07748394227eef5feb0db394d6d
unknown <jorge@.(none)>
2013-11-14 05:06:24 -0500

    Oh yeah!!!

--- application/views/paciente/paciente_view_enfermedad.php
+++ application/views/paciente/paciente_view_enfermedad.php
@@ -1,3 +1,89 @@
 <div id="enfermedad_actual" class="content_tab">
-	<h1>Enfermedad aqui!!!</h1>
+	<div class="area">
+		<div class="posicion1" >
+			<div style="float:left; width:519px; height:60px;">
+				<label>Tiempo de Enfermedad (T.E) :</label>
+				<br/>
+				<input type="text" name="txthospitalizacion" value=""  placeholder="" required="" />
+			</div>
+		</div>
+		<div class="posicion1" >
+			<div style="float:left; width:519px; height:60px;">
+				<label>SP :</label>
+				<br/>
+				<input type="text" name="txthnocivos" value="" placeholder="" required=""/>
+			</div>
+		</div>
+		<br/><br/><br/>
+		<div class="posicion1" >
+			<div style="float:left; width:1054px; height:auto;">
+				<label>Relato :</label>
+				<br/>
+				<textarea rows="3" cols="127"></textarea>
+			</div>
+		</div>
+		<br><br><br><br><br>
+		<div class="posicion1" >
+			<div style="float:left; width:1044px; height:auto;">
+				<table class="tbl-funciones" style="width:100%;border: 1px solid #ABADB3; border-collapse: collapse;">
+					<tr class="row1">
+						<td width="17%">FUNCIONES VITALES:</td>
+						<td width="16%">F.C. <input type="text"/></td>
+						<td width="17%">P.A. <input type="text"/> </td>
+						<td width="17%">F.R. <input type="text"/> </td>
+						<td width="16%">T. <input type="text"/> </td>
+						<td width="17%">PESO <input type="text"/> </td>
+					</tr>
+					<tr class="row2">
+						<td colspan="2" width="33%">GENERAL <input type="text"/></td>
+						<td colspan="2" width="33%">CABEZA: <input type="text"/></td>
+						<td colspan="2" width="34%">CUELLO: <input type="text"/></td>
+					</tr>
+					<tr class="row3">
+						<td colspan="3" width="50%">TORAX Y PULMONES: <input type="text"/></td>
+						<td colspan="3" width="50%">CARDIOVASCULAR <input type="text"/></td>
+					</tr>
+					<tr>
+						<td colspan="2" width="33%" valign="middle" align="center">ABDOMEN</td>
+						<td colspan="4" width="67%">
+							<textarea style="border: 0; height: 30px; width: 690px;"></textarea>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" valign="middle" align="center">GENITO-UNITARIO</td>
+						<td colspan="4">
+							<textarea style="border: 0; height: 30px; width: 690px;"></textarea>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" valign="middle" align="center">LOCOMOTOR Y NEUROLOGICO</td>
+						<td colspan="4">
+							<textarea style="border: 0; height: 30px; width: 690px;"></textarea>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" valign="middle" align="center">Dx.</td>
+						<td colspan="4">
+							<textarea style="border: 0; height: 30px; width: 690px;"></textarea>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" valign="middle" align="center">TRATAMIENTO</td>
+						<td colspan="4">
+							<textarea style="border: 0; height: 30px; width: 690px;"></textarea>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+		<div class="posicion1" >
+			<div style="float:left; width:1044px; height:auto;">
+				<label>Exámenes Auxiliares :</label>
+				<br/>
+				<textarea rows="3" cols="127"></textarea>
+			</div>
+		</div>
+		<br/><br/><br/>
+	</div>
 </div>
